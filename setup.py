d52651a761e70f18ca48f8f65acd9c2bef52d0be
LukeCS420 <lukecollishaw@gmail.com>
2020-12-04 11:34:47 +0000

    First commit

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(
+    name='Luke Collishaw-Schepman',
+    version='1.0.0',
+    packages=['covid-alarm-clock',],
+    license='Apache License, Version 2',
+    long_description=open('README.md').read(),
+)
