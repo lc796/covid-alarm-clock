3661be195b0e6f9a09adee5488fa0cd87756b34c
LukeCS420 <lukecollishaw@gmail.com>
2020-12-04 11:41:22 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,9 +1,21 @@
-from distutils.core import setup
+import setuptools
 
-setup(
-    name='Luke Collishaw-Schepman',
-    version='1.0.0',
-    packages=['covid-alarm-clock',],
-    license='Apache License, Version 2',
-    long_description=open('README.md').read(),
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="LukeCollishaw",
+    version="1.0.0",
+    author="Luke Collishaw-Schepman",
+    author_email="lc796@exeter.ac.uk",
+    description="COVID Alarm Clock",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/lc796/covid-alarm-clock",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
 )
